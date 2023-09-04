548942a639f3a72566e08da01394a8d71eafe0c9
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-04 19:08:42 +0530

    Time: 4 ms (73.53%), Space: 43.9 MB (72.5%) - LeetHub

--- 2159-two-out-of-three/2159-two-out-of-three.java
+++ 2159-two-out-of-three/2159-two-out-of-three.java
@@ -2,23 +2,20 @@ class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         HashSet<Integer> res = new HashSet<>();
 
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
         for(int el : nums1)
-            set1.add(el);
+            set.add(el);
         
-        for(int el : nums2){
-            if(set1.contains(el))
+        for(int el : nums2)
+            if(set.contains(el))
                 res.add(el);
-            set2.add(el);
-        }
         
-        for(int el : set2)
-            set1.add(el);
+        for(int el : nums2)
+            set.add(el);
         
         for(int el : nums3)
-            if(set1.contains(el))
+            if(set.contains(el))
                 res.add(el);
 
         List<Integer> list = new ArrayList<>();
