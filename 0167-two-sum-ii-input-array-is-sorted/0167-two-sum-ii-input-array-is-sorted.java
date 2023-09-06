c1c07194495231c1153d900c02d23f80271535b5
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-07 02:45:42 +0530

    Time: 0 ms (100%), Space: 46 MB (8.43%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -9,12 +9,14 @@ class Solution {
             if(sum == target)
                 return new int[]{i+1, j+1};
             else if(sum < target){
+			// if nums[i] + nums[j] is less than target, then we have to increment i, to optimize it, we are also checking, if nums[mid] + nums[j] is still less than target, which means that answer is not present in 1st part of array (before mid). so just i = mid.
                 if(nums[mid] + nums[j] < target)
                     i = mid;
                 else
                     i++;
             }
             else{
+				// similar explaination for this one.
                 if(nums[mid] + nums[i] > target)
                     j = mid;
                 else
