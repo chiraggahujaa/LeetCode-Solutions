3b33dca52f4dd5debac12660fc2ac5c4ecefd60f
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-11 23:28:43 +0530

    Time: 2 ms (71.04%), Space: 43.4 MB (99.45%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -4,43 +4,54 @@ class Solution {
     public void solve(char[][] board) {
         this.n = board.length;
         this.m = board[0].length;
+
         this.moves = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
 
-        for(int i=0; i<n; i++){
-            if(board[i][0] == 'O')
-                dfs(i, 0, board);
+        // capture surrounded region or we can say : (reverse logic)
+        // don't capture unsurrounded region
 
-            if(board[i][m-1] == 'O')
-                dfs(i, m-1, board);
+        // what's unsurrounded region ->
+        // every on the borded and everything connected to it for the value 'O'.
+
+        boolean[][] vis = new boolean[n][m];        
+
+        // traverse through the border and run dfs on the cell with value 'O' and make all these cells to 'T'.
+        for(int i=0; i<n; i++){
+            if(!vis[i][0] && board[i][0] == 'O')
+                dfs(i, 0, vis, board);
+            if(!vis[i][m-1] && board[i][m-1] == 'O')
+                dfs(i, m-1, vis, board);
         }
         for(int j=0; j<m; j++){
-            if(board[0][j] == 'O')
-                dfs(0, j, board);
-            if(board[n-1][j] == 'O')
-                dfs(n-1, j, board);
+            if(!vis[0][j] && board[0][j] == 'O')
+                dfs(0, j, vis, board);
+            if(!vis[n-1][j] && board[n-1][j] == 'O')
+                dfs(n-1, j, vis, board);
         }
 
+        // at end, traverse and make left 'O' to 'X'.
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(board[i][j] == 'O')
                     board[i][j] = 'X';
-                else if(board[i][j] == 'T')
+                if(board[i][j] == 'T')
                     board[i][j] = 'O';
             }
         }
     }
-    public void dfs(int i, int j, char[][] board){
+    public void dfs(int i, int j, boolean[][] vis, char[][] board){
+        vis[i][j] = true;
         board[i][j] = 'T';
 
         for(int[] moveTo : moves){
             int ii = i + moveTo[0];
             int jj = j + moveTo[1];
 
-            if(isValid(ii, jj) && board[ii][jj] == 'O')
-                dfs(ii, jj, board);
+            if(isValid(ii, jj) && !vis[ii][jj] && board[ii][jj] == 'O')
+                dfs(ii, jj, vis, board);
         }
     }
-    private boolean isValid(int i, int j){
+    public boolean isValid(int i, int j){
         return i>=0 && j>=0 && i<n && j<m;
     }
 }
