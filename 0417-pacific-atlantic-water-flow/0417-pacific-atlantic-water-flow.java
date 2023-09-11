251b82d8ef39e0f83b07785856f0724d2e0c8b6f
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-11 23:03:10 +0530

    Time: 4 ms (98.26%), Space: 44.6 MB (70.51%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,22 +1,30 @@
 class Solution {
     int n, m;
-    boolean isPac, isAtl;
+    boolean[][] pac, atl;
     int[][] moves;
     public List<List<Integer>> pacificAtlantic(int[][] nums) {
+        // burte force
         this.n = nums.length;
         this.m = nums[0].length;
         this.moves = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        this.pac = new boolean[n][m];
+        this.atl = new boolean[n][m];
 
         List<List<Integer>> res = new ArrayList<>();
 
+        for(int i=0; i<n; i++)
+            dfs(i, 0, pac, nums);
+        for(int j=0; j<m; j++)
+            dfs(0, j, pac, nums);
+        
+        for(int i=0; i<n; i++)
+            dfs(i, m-1, atl, nums);
+        for(int j=0; j<m; j++)
+            dfs(n-1, j, atl, nums);
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                this.isPac = false;
-                this.isAtl = false;
-
-                dfs(i, j, new boolean[n][m], nums);
-
-                if(isPac && isAtl)
+                if(pac[i][j] && atl[i][j])
                     res.add(Arrays.asList(i, j));
             }
         }
@@ -30,17 +38,10 @@ class Solution {
             int ii = i + moveTo[0];
             int jj = j + moveTo[1];
 
-            if(ii < 0 || jj < 0){
-                isPac = true;
+            if(ii < 0 || jj < 0 || ii >= n || jj >= m)
                 continue;
-            }
-
-            if(ii >= n || jj >= m){
-                isAtl = true;
-                continue;
-            }
-
-            if(!vis[ii][jj] && nums[ii][jj] <= nums[i][j]){
+            
+            if(!vis[ii][jj] && nums[ii][jj] >= nums[i][j]){
                 dfs(ii, jj, vis, nums);
             }
         }
