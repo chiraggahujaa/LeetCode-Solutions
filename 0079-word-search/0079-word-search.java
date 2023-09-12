5ea9ce338220755a64e5bf81e971869622b0267f
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-12 07:43:49 +0530

    Time: 159 ms (51.37%), Space: 40.4 MB (75.43%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,39 +1,34 @@
 class Solution {
     int n, m;
     int[][] moves;
+    boolean[][] vis;
     public boolean exist(char[][] board, String word) {
         this.n = board.length;
         this.m = board[0].length;
-
         this.moves = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        this.vis = new boolean[n][m];
 
-        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                boolean[][] vis = new boolean[n][m];
-                boolean[][] inRec = new boolean[n][m];
-                if(board[i][j] == word.charAt(0))
-                    if(dfs(i, j, 1, vis, inRec, board, word))
-                        return true;
+                if(board[i][j] == word.charAt(0) && f(1, i, j, board, word))
+                    return true;
             }
         }
 
         return false;
     }
-    public boolean dfs(int i, int j, int ind, boolean[][] vis, boolean[][] inRec, char[][] board, String word){
+    public boolean f(int ind, int i, int j, char[][] board, String word){
         if(ind == word.length())
             return true;
-            
+        
         vis[i][j] = true;
 
         for(int[] moveTo : moves){
             int ii = i + moveTo[0];
             int jj = j + moveTo[1];
 
-            
-
-            if(isValid(ii, jj)){
-                if(!vis[ii][jj] && word.charAt(ind) == board[ii][jj] && dfs(ii, jj, ind+1, vis, inRec, board, word))
+            if(isValid(ii, jj) && !vis[ii][jj] && board[ii][jj] == word.charAt(ind)){
+                if(f(ind+1, ii, jj, board, word))
                     return true;
             }
         }
@@ -41,7 +36,7 @@ class Solution {
         vis[i][j] = false;
         return false;
     }
-    public boolean isValid(int i, int j){
+    private boolean isValid(int i, int j){
         return i>=0 && j>=0 && i<n && j<m;
     }
 }
