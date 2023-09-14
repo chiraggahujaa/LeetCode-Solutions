3309e9eb29988ef4e80d0e38efa8a991a75c4c78
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-14 14:44:42 +0530

    Time: 0 ms (100%), Space: 40 MB (94.24%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,34 +1,28 @@
 class Solution {
-    int n;
-    int[][] dp;
     public boolean checkValidString(String s) {
-        this.n = s.length();
-        this.dp = new int[n][n];
+        int n = s.length();
 
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
+        int leftMin = 0, leftMax = 0;
+        for(int i=0; i<n; i++){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                leftMin++;
+                leftMax++;
+            }
+            else if(ch == ')'){
+                leftMin = Math.max(leftMin-1, 0);
+                leftMax--;
 
-        return f(0, 0, s);
-    }
-    public boolean f(int i, int size, String s){
-        if(size < 0)
-            return false;
-        if(i >=n)
-            return size == 0;
+                if(leftMax < 0)
+                    return false;
+            }
+            else{
+                leftMin = Math.max(leftMin-1,0);
+                leftMax++;
+            }
+        }
 
-        if(dp[i][size] != -1)
-            return dp[i][size] == 1;
-
-        char ch = s.charAt(i);
-        boolean res = false;
-        if(ch == '(')
-            res = f(i+1, size+1, s);
-        else if(ch == ')')
-            res = f(i+1, size-1, s);
-        else
-            res = (f(i+1, size, s) || f(i+1, size+1, s) || f(i+1, size-1, s));
-
-        dp[i][size] = res ? 1 : 0;
-        return res;
+        return leftMin == 0;
     }
 }
