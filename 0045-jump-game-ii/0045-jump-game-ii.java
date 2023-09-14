41d7a9e400a7a6392e2ca3225374b7c3b387a898
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-14 11:41:10 +0530

    Time: 1 ms (99.26%), Space: 44 MB (97.75%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -2,30 +2,20 @@ class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
 
-        boolean[] vis = new boolean[n];
-        Queue<Integer> q = new  ArrayDeque<>();
-
-        q.offer(0);
-        vis[0] = true;
-
+        int l = 0, r = 0;
         int count = 0;
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int x=0; x<size; x++){
-                int i = q.poll();
-                if(i >= n-1)
-                    return count;
 
-                for(int k=1; k<=nums[i]; k++){
-                    if(i+k<n && !vis[i+k]){
-                        q.offer(i+k);
-                        vis[i+k] = true;
-                    }
-                }
-            }
-            count ++;
+        while(r <= n-2){
+            int farthest = 0;
+            for(int i=l; i<=r; i++)
+                farthest = Math.max(farthest, i + nums[i]);
+
+            l = r+1;
+            r = farthest;
+
+            count++;
         }
 
-        return -1;
+        return count;
     }
 }
