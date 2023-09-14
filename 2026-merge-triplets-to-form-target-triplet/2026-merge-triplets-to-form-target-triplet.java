be4b7bdbdb629d161343f863071338ed3d814b30
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-14 13:46:49 +0530

    Time: 6 ms (45.01%), Space: 122.4 MB (40.69%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
@@ -3,8 +3,11 @@ class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] t) {
         int[] res = new int[3];
         for(int[] s : triplets){
-            if(s[0] <= t[0] && s[1] <= t[1] && s[2] <= t[2])
-                res = new int[]{Math.max(s[0], res[0]), Math.max(s[1], res[1]), Math.max(s[2], res[2])};
+            if(s[0] <= t[0] && s[1] <= t[1] && s[2] <= t[2]){
+                res[0] = Math.max(res[0], s[0]);
+                res[1] = Math.max(res[1], s[1]);
+                res[2] = Math.max(res[2], s[2]);
+            }
         }
 
         return Arrays.equals(res, t);
