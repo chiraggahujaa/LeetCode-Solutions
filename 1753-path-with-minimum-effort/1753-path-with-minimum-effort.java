99032d82f1faaa1a406345c7a485d152f24640ba
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-16 08:21:37 +0530

    Time: 154 ms (8.97%), Space: 44.7 MB (17.88%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -8,28 +8,29 @@ class Pair{
 }
 class Solution {
     int n, m;
-    int[][] moves;
-    public int minimumEffortPath(int[][] grid) {
-        this.n = grid.length;
-        this.m = grid[0].length;
+    int[][] moves, res;
+    public int minimumEffortPath(int[][] nums) {
+        this.n = nums.length;
+        this.m = nums[0].length;
 
-        this.moves = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+        this.moves = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        this.res = new int[n][m];
 
         PriorityQueue<Pair> pq = new PriorityQueue<>(
             (a,b) -> a.diff - b.diff
-        );
-        int[][] res = new int[n][m];
+        ); // min heap
 
         for(int[] row : res)
             Arrays.fill(row, Integer.MAX_VALUE);
-        pq.offer(new Pair(0,0,0));
+
         res[0][0] = 0;
+        pq.offer(new Pair(0, 0, 0));
 
         int maxDiff = 0;
-
         while(!pq.isEmpty()){
             Pair p = pq.poll();
             int i = p.i, j = p.j, diff = p.diff;
+
             maxDiff = Math.max(maxDiff, diff);
 
             if(i == n-1 && j == m-1)
@@ -42,18 +43,17 @@ class Solution {
                 if(!isValid(ii, jj))
                     continue;
 
-                int currDiff = Math.abs(grid[ii][jj] - grid[i][j]);
-
-                if(currDiff < res[ii][jj]){
+                int currDiff = Math.abs(nums[i][j] - nums[ii][jj]);
+                if(res[ii][jj] > diff){
                     res[ii][jj] = currDiff;
-                    pq.offer(new Pair(ii, jj, res[ii][jj]));
+                    pq.offer(new Pair(ii, jj, currDiff));
                 }
             }
         }
 
-        return maxDiff;
+        return -1;
     }
-    public boolean isValid(int i, int j){
+    private boolean isValid(int i, int j){
         return i>=0 && j>=0 && i<n && j<m;
     }
 }
