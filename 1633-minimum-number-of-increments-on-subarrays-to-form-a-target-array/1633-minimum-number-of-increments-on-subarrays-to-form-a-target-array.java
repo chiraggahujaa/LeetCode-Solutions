484c253490a89be75aa67ef4e8f37d3845782ea3
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-26 22:13:27 +0530

    Time: 4 ms (33.94%), Space: 55.5 MB (52.12%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -1,3 +1,21 @@
+// Logic -> Greedy
+// [3,1,1,2]
+
+// at i=0, we must need atleast 3 operation
+// that operation will be useful to reduce
+// [3,1,1,2] -> [2,0,0,1] -> [1,0,0,1] -> [0,0,0,1]
+
+// at i=2 and i=3, we no need any more operation
+
+// at i=4, we must need atleast 1 operation
+// that operation will be useful to reduce [0,0,0,1] -> [0,0,0,0]
+
+// If we carefully notice, for each index we need abs(nums[i] - nums[i-1]) operations. (Intially nums[0] operations)
+// How? 
+// Initally at i=0, 3 is for sure required since to reduce a value 3 to 0, we need atleast 3 operations
+// at i=1 and i=2, we require 1-3 = -2 = 0 operations are required => cz they are already reduced by intial operation
+// at i=3, we require 2-1 = 1 operation to reduce 1 to 0 at the end.
+
 class Solution {
     public int minNumberOperations(int[] target) {
         int res = target[0];
