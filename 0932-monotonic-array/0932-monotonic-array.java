eb670ea23c3f52e5a84bf65f908742fe294a05af
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-09-29 20:08:47 +0530

    Time: 1 ms (99.82%), Space: 55.9 MB (35.23%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -4,6 +4,8 @@ class Solution {
 
         boolean isInc = true, isDec = true;
         for(int i=1; i<n; i++){
+            if(!(isInc || isDec))
+                return false;
             if(nums[i] < nums[i-1])
                 isInc = false;
             if(nums[i] > nums[i-1])
