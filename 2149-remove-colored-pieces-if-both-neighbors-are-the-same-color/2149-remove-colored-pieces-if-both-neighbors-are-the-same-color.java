43da882a6f03232ad5d372fbc02ee544efa1655b
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-10-02 15:17:58 +0530

    Time: 11 ms (77.64%), Space: 44.2 MB (41.35%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
-        // make 2 priority queue
-        // groups of A -> 3,1
-        // groups of B -> 6
+        // we can maintain number of chance that alice have and number of chance that bob has.
+        // if aliceChance <= bobChance -> false, else -> true
 
-        // alice moves
-        // A -> 2,1
-        
-        // bob moves
-        // B -> 5
+        // "ABBBBBBBAAA"
+        // aliceChance = pos(1-2) + pos(3-2) = 0 + 1 = 1 --> pos() means take only postive, if negitive then 0
+        // bobChance = pos(6-2) = 4
 
-        // alice moves
-        // can't => alice looses
+        // aliceChance <= bobChance -> false
 
         int n = colors.length();
-        PriorityQueue<Integer> p = new PriorityQueue<>(Collections.reverseOrder()); // max heap
-        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder()); // max heap
+        int aliceChance = 0, bobChance = 0;
 
         int i=0;
         while(i<n){
@@ -26,27 +21,11 @@ class Solution {
                 i++;
             }
             if(ch == 'A')
-                p.offer(count);
+                aliceChance += Math.max(count-2, 0);
             else
-                q.offer(count);
+                bobChance += Math.max(count-2, 0);
         }
 
-        boolean aliceTurn = true;
-        while(true){
-            if(aliceTurn){
-                if(p.isEmpty() || p.peek() < 3)
-                    return false;
-                p.offer(p.poll()-1);
-
-                aliceTurn = false;
-            }
-            else{
-                if(q.isEmpty() || q.peek() < 3)
-                    return true;
-                q.offer(q.poll()-1);
-
-                aliceTurn = true;
-            }
-        }
+        return aliceChance > bobChance;
     }
 }
