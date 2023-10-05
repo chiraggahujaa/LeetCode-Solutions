bb4ba8f61a92ec51b328ef01391d7d52280e2ca9
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-10-05 14:22:03 +0530

    Time: 2 ms (98.52%), Space: 46.5 MB (64.44%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        // note - max number of majority element are 2
+        // we have to choose 2 contender for the majority elements
+
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int el : nums)
-            map.put(el, map.getOrDefault(el, 0)+1);
+        int num1 = -1, num2 = -1, count1 = 0, count2 = 0;
+        for(int el : nums){
+            if(count1 == 0 && el != num2){
+                num1 = el;
+                count1 = 1;
+            }
+            else if(count2 == 0 && el != num1){
+                num2 = el;
+                count2 = 1;
+            }
+            else if(num1 == el){
+                count1++;
+            }
+            else if(num2 == el){
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        int c1 = 0, c2 = 0;
+        for(int el : nums){
+            if(el == num1)
+                c1++;
+            else if(el == num2)
+                c2++;
+        }
 
         int reqLen = n/3;
-        List<Integer> list = new ArrayList<>();
-        for(Map.Entry<Integer, Integer> e : map.entrySet()){
-            int freq = e.getValue();
-            if(freq > reqLen)
-                list.add(e.getKey());
-        }
+        if(reqLen < c1)
+            list.add(num1);
+        if(reqLen < c2)
+            list.add(num2);
 
         return list;
     }
