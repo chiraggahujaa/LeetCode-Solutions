f8d6181706a7218e56295500802941ee6e52bc0b
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-10-05 15:34:03 +0530

    Time: 76 ms (5.7%), Space: 43.2 MB (68.74%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -1,15 +1,5 @@
-/**
- * @param {Function[]} functions
- * @return {Function}
- */
 var compose = function(functions) {
-    
 	return function(x) {
         return functions.reduceRight((val, fn) => fn(val), x);
     }
 };
-
-/**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
