cdada97043a8809288b8509fa5183e8c145b42fb
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-10-06 12:25:20 +0530

    Time: 0 ms (100%), Space: 39.3 MB (39.77%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,8 +1,16 @@
 class Solution {
     public int integerBreak(int n) {
+        // logic -- 
+        // every integer n can be broken down into 1 to n
+        // so by dp we try for each of them, i.e. for every integer we break that down into 1 to n.
+        // i.e. if we break n into i and n-i then product is i*f(n-i)
+        // and we take maximum of all these values
+
         // edge cases (bz must break into 2)
-        // 2 -- 1*1
-        // 3 -- 2*1
+        // only for 2 and 3 the product after breaking is less than their value. that's value we have to handle them diffrently
+        // 2 -- 1*1 < 2
+        // 3 -- 2*1 < 3
+
         if(n == 2)
             return 1;
         if(n == 3)
