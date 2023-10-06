1df58d62674e285c504697d8bdf41cb0745fc03c
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-10-06 12:31:22 +0530

    Time: 0 ms (100%), Space: 39.1 MB (51.27%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,37 +1,23 @@
 class Solution {
     public int integerBreak(int n) {
-        // logic -- 
-        // every integer n can be broken down into 1 to n
-        // so by dp we try for each of them, i.e. for every integer we break that down into 1 to n.
-        // i.e. if we break n into i and n-i then product is i*f(n-i)
-        // and we take maximum of all these values
+        // Approch -- greedy 
+        // we must note that we will get the maximum product value when we split into sum of 3
+        // 12 -- 6*6 = 36 or 4*4*4 = 64 or 3*3*3*3 = 81 => max = 81
 
         // edge cases (bz must break into 2)
         // only for 2 and 3 the product after breaking is less than their value. that's value we have to handle them diffrently
         // 2 -- 1*1 < 2
         // 3 -- 2*1 < 3
 
-        if(n == 2)
-            return 1;
-        if(n == 3)
-            return 2;
+        if(n <= 3)
+            return n-1;
 
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-
-        return f(n, dp);
-    }
-    public int f(int n, int[] dp){
-        if(n == 0)
-            return 1;
-
-        if(dp[n] != -1)
-            return dp[n];
-
-        int prod = 1;
-        for(int i=1; i<=n; i++)
-            prod = Math.max(prod, i * f(n-i, dp));
-
-        return dp[n] = prod;
+        int res = 1;
+        while(n > 4){
+            res *= 3;
+            n -= 3;
+        }
+        return res*n;
+    
     }
 }
