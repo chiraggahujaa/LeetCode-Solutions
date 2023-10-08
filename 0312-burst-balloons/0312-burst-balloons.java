698e1f973325152c254cc4c19bf0d45dcc47f8fd
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-10-09 01:56:21 +0530

    Time: 97 ms (54.62%), Space: 41.4 MB (50.76%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,35 +1,41 @@
 class Solution {
-    int[] nums;
     int n;
-    int[][] dp;
-    public int maxCoins(int[] oldNums) {
-        this.n = oldNums.length;
-        this.nums = new int[n+2];
-        this.dp = new int[n+1][n+1];
+    public int maxCoins(int[] nums) {
+        // we can start from the last burst to first burst
+        // [3,1,5,8]
+        // let's say we have 8 remaining at last => res += 1*8*1
+        // [8] => let's say we have 3 at 2nd last => res += 1*3*8
+        // [3,8] => let's say we have 5 at 3rd last => res += 3*5*8
+        // [3,5,8] => let's say we have 1 at 4th last => res += 3*1*5
+        // so res = 167
 
+        // [1,3,1,5,8,1] -- extened array
+        // i=0, j=n+1, k can be between i and j (both exclusive)
+        // choose k, then f(i, k) + f(k, j) + arr[i]*arr[k]*arr[j] => get max of this
+        
+        this.n = nums.length;
+        int[] arr = new int[n+2];
+        arr[0] = 1; arr[n+1] = 1;
+        for(int i=1; i<=n; i++)
+            arr[i] = nums[i-1];
+
+        int[][] dp = new int[n+2][n+2];
         for(int[] row : dp)
             Arrays.fill(row, -1);
 
-        nums[0] = 1;
-        for(int i=0; i<n; i++)
-            nums[i+1] = oldNums[i];
-        nums[n+1] = 1;
-
-        return f(1, n);
+        return f(0, n+1, arr, dp);
     }
-    public int f(int start, int end){
-        if(start > end)
+    public int f(int i, int j, int[] nums, int[][] dp){
+        if(i+1 >= j)
             return 0;
 
-        if(dp[start][end] != -1)
-            return dp[start][end];
-        
-        int res = 0;
-        for(int j=start; j<=end; j++){
-            int curr = nums[start-1] * nums[end+1] * nums[j] + f(start, j-1) + f(j+1, end);
-            res = Math.max(res, curr);
-        }
+        if(dp[i][j] != -1)
+            return dp[i][j];
 
-        return dp[start][end] = res;
+        int res = 0;
+        for(int k=i+1; k<j; k++)
+            res = Math.max(nums[i]*nums[k]*nums[j] + f(i, k, nums, dp) + f(k, j, nums, dp), res);
+        
+        return dp[i][j] = res;
     }
 }
