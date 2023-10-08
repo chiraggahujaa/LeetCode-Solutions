565dbb404d666e36ddc633fe68372e56dd3cb08b
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-10-08 18:40:54 +0530

    Time: 16 ms (79.55%), Space: 46.9 MB (23.12%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,56 +1,39 @@
 class Solution {
     int n1, n2;
-    int[][] dp;
-    public boolean isMatch(String s, String t) {
-        this.n1 = s.length();
-        this.n2 = t.length();
+    public boolean isMatch(String s, String p) {
+        // O(n^2) Approch
+        // "*" -> f(i+1, j) or f(i+1, j+1)
+        // "?" -> f(i+1, j+1)
+        // "aab", "c*a*b" -> c!=a -> false
 
-        this.dp = new int[n1][n2];
+        this.n1 = s.length();
+        this.n2 = p.length();
+
+        int[][] dp = new int[n1][n2];
         for(int[] row : dp)
             Arrays.fill(row, -1);
 
-        return f(0, 0, s, t);
+        return f(0, 0, s, p, dp); 
     }
-    public boolean f(int i, int j, String s, String t){
+    public boolean f(int i, int j, String s, String p, int[][] dp){
         if(i == n1){
-            if(j == n2)
-                return true;
-            
-            while(j < n2){
-                if(t.charAt(j) != '*')
-                    return false;
+            while(j < n2 && p.charAt(j) == '*')
                 j++;
-            }
-
-            return true;
+            return j == n2;
         }
-
-        if(j == n2)
+        if(j >= n2)
             return false;
 
         if(dp[i][j] != -1)
             return dp[i][j] == 1;
-        
-        char ch1 = s.charAt(i);
-        char ch2 = t.charAt(j);
 
-        if(ch2 == '*'){
-            for(int k=i; k<=n1; k++){
-                if(f(k, j+1, s, t)){
-                    dp[i][j] = 1;
-                    return true;
-                }
-            }
-        }
-        else{
-            boolean match = (ch2 == '?') || (ch1 == ch2);
-            if(match && f(i+1, j+1, s, t)){
-                dp[i][j] = 0;
-                return true;
-            }
-        }
-        
-        dp[i][j] = 0;
-        return false;
+        boolean res = false;
+        if(p.charAt(j) == '*')
+            res = f(i, j+1, s, p, dp) || f(i+1, j, s, p, dp);
+        else if(p.charAt(j) == '?' || s.charAt(i) == p.charAt(j))
+            res = f(i+1, j+1, s, p, dp);
+
+        dp[i][j] = (res ? 1 : 0);
+        return res;
     }
 }
