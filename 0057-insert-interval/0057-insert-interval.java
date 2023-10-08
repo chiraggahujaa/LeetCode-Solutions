9954c0f937d31b3b081592adbf8ee9db6c6595b4
chiraggahujaa <73935212+chiraggahujaa@users.noreply.github.com>
2023-10-08 22:35:19 +0530

    Time: 3 ms (23.84%), Space: 44 MB (42.15%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,67 +1,48 @@
 class Solution {
-    int n;
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        this.n = intervals.length;
-
-        // binary search -> find insertion index
-        int ind = findInsertionIndex(intervals, newInterval);
-
-        // insert value at it's index
-        intervals = insertAtIndex(ind, intervals, newInterval);
-
-        // merge overlapping intervals
-        return merge(intervals);
-    }
-    
-    public int[][] merge(int[][] nums) {
-        Arrays.sort(nums, (a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-
-        int n = nums.length;
-        List<int[]> list = new ArrayList<>();
-
-        for(int i=0; i<n-1; i++){
-            int a = nums[i][0], b = nums[i][1], c = nums[i+1][0], d = nums[i+1][1];
-            
-            if(b >= c){
-                nums[i+1][0] = nums[i][0];
-                nums[i+1][1] = Math.max(nums[i+1][1], nums[i][1]);
-            }
-            else{
-                list.add(new int[]{nums[i][0], nums[i][1]});
-            }
-        }
-        list.add(new int[]{nums[n-1][0], nums[n-1][1]});
-
-        int[][] res = new int[list.size()][2];
-        int ind = 0;
-        for(int[] arr : list){
-            res[ind++] = arr;
-        }
-
-        return res;
-    }
-    private int findInsertionIndex(int[][] intervals, int[] newInterval){
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++)
-            arr[i] = intervals[i][0];
-
-        int ind = Arrays.binarySearch(arr, newInterval[0]);
-        if(ind < 0)
-            ind = Math.abs(ind) - 1;
-
-        return ind;
-    }
-    private int[][] insertAtIndex(int ind, int[][] intervals, int[] newInterval){
+        int n = intervals.length;
         int[][] arr = new int[n+1][2];
 
-        for(int i=0; i<ind; i++)
+        for(int i=0; i<n; i++)
             arr[i] = intervals[i];
-        
-        arr[ind] = newInterval;
+        arr[n] = newInterval;
 
-        for(int i=ind+1; i<=n; i++)
-            arr[i] = intervals[i-1];
+        Arrays.sort(arr, (a,b) -> a[0] - b[0]);
+
+        return merge(arr);
+    }
+    public int[][] merge(int[][] intervals) {
+        // [a,b] [c,d]
+        // [1,5] [2,3] -> [1,5]
+        // [1,3] [2,4] -> [1,4]
+        // [a, max(b,d)]
+
+        // b < c then do nothing
         
-        return arr;
+        int n = intervals.length;
+
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
+
+        for(int i=1; i<n; i++){
+            int a = list.get(list.size()-1)[0], b = list.get(list.size()-1)[1], c = intervals[i][0], d = intervals[i][1];
+
+            if(b < c){
+                list.add(intervals[i]);
+                continue;
+            }
+            
+            list.remove(list.size()-1);
+            list.add(new int[]{a, Math.max(b,d)});
+        }
+
+        int len = list.size();
+        int[][] res = new int[len][2];
+        for(int i=0; i<len; i++)
+            res[i] = list.get(i);
+        
+        return res;            
     }
 }
